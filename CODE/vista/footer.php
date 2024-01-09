92d02eabe32c4ae8f7dfcbc336273271bbe1c1bb
angelcabgom <angelm.cabgom.1@educa.jcyl.es>
2024-01-09 11:33:13 +0100

    El resto del codigo inicial

---
+++ CODE/vista/footer.php
@@ -0,0 +1,27 @@
+<footer class="footer mt-auto bg-dark text-white text-center">
+    <div class="container">
+        <div class="row">
+            <div class="col text-start">
+                <span href="#" class="text-white">&copy; Autohaus Leon</span>
+            </div>
+            <div class="col">
+                <span href="#" class="text-white">987 654 321</span>
+            </div>
+            <div class="col">
+                <span href="#" class="text-white">autohausleon@gmail.com</span>
+            </div>
+            <div class="col">
+                <span href="#" class="text-white">Gran Vía de San Marcos, 59, 24001 León</span>
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- Bootstrap JavaScript Libraries -->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+</script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+</script>
+</body>
+
+</html>
