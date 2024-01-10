57a304110037a9362931b660b4985d7c932585c7
angelcabgom <angelm.cabgom.1@educa.jcyl.es>
2024-01-10 09:03:00 +0100

    headerFalseLogin

---
+++ CODE/vista/headerTrueLogin.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Autohaus</title>
+    <!-- Required meta tagss -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="shortcut icon" href="img/logo.png" type="image/x-icon">
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link href="https://db.onlinewebfonts.com/c/e9de4f4d373ef5c6d9cf52490816ff5d?family=BMW+Type+Web+Light+All" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <!-- header -->
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container-fluid">
+            <!-- Logo y nombre navbarr -->
+            <a href="main.php" class="btn custom-link d-flex align-items-center" id="logo">
+                <div id="tituloCabezera" class="mx-2 my-0">TrekWikia</div>
+            </a>
+            <!-- Items navbarr -->
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="custom-navbar-nav navbar-nav align-items-center">
+                    <li class="custom-nav-item-search nav-item mx-5">
+                        <div class="search-container">
+                            <span class="material-symbols-outlined">search</span>
+                            <input class="custom-search-bar form-control" type="search" placeholder="Buscar carreras" aria-label="Search">
+                        </div>
+                    </li>
+                    <li class="custom-dropdown-menu nav-item dropdown">
+                        <a class="explore-link nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Explora
+                        </a>
+                        <!-- Dropdown menu -->
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <div class="row">
+                                <div class="col-6">
+                                    <div class="dropdown-item text-start">
+                                        <a href="#">Rutas en España</a>
+                                    </div>
+                                </div>
+                                <div class="col-6">
+                                    <div class="dropdown-item text-start">
+                                        <a href="#">Rutas en Francia</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-6">
+                                    <div class="dropdown-item text-start">
+                                        <a href="#">Rutas en Alemania</a>
+                                    </div>
+                                </div>
+                                <div class="col-6">
+                                    <div class="dropdown-item text-start">
+                                        <a href="#">Rutas en Suiza</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </ul>
+                    </li>
+                    <ul class="navbar-nav ms-auto">
+                        </li>
+                        <!-- Login Button -->
+                        <li class="custom-login nav-item mx-5">
+                            <a class="nav-link" href="login.php">Login</a>
+                        </li>
+                        <!-- Register Button -->
+                        <li class="custom-register nav-item mx-2">
+                            <a class="nav-link" href="register.php">Register</a>
+                        </li>
+                    </ul>
+                </ul>
+            </div>
+        </div>
+    </nav>
