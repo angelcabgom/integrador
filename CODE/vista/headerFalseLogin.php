8ad7d679833972a9956ffcd946faf6e5b15cc54a
angelcabgom <angelm.cabgom.1@educa.jcyl.es>
2024-01-10 16:47:07 +0100

    Cambio de title

--- CODE/vista/headerFalseLogin.php
+++ CODE/vista/headerFalseLogin.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>Autohaus</title>
+    <title>TrekWikia</title>
     <!-- Required meta tagss -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -63,6 +63,8 @@
                             </div>
                         </ul>
                     </li>
+
+                    <!-- Caambiar el hover se ve horrible -->
                     <ul class="navbar-nav ms-auto">
                         </li>
                         <li class="custom-login nav-item mx-5">
--- CODE/vista/headerTrueLogin.php
+++ CODE/vista/headerTrueLogin.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>Autohaus</title>
+    <title>TrekWikia</title>
     <!-- Required meta tagss -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
