448671e848e9b66cd5c6fea9684dfb632788ee6d
angelcabgom <angelm.cabgom.1@educa.jcyl.es>
2024-01-25 14:07:30 +0100

    Carrusel de mapas todavia con muchisimos fallos

--- CODE/vista/headGlobal.php
+++ CODE/vista/headGlobal.php
@@ -9,10 +9,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="shortcut icon" href="img/logo.png" type="image/x-icon">
     <!-- Bootstrap CSS v5.2.1 -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    <link href="https://db.onlinewebfonts.com/c/e9de4f4d373ef5c6d9cf52490816ff5d?family=BMW+Type+Web+Light+All" rel="stylesheet">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.css">
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet-gpx/1.7.0/gpx.min.js"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
 
     <!-- Array con todos los estilos en comun -->
     <?php $estilosComunes = [
--- CODE/vista/paginaPrincipal.php
+++ CODE/vista/paginaPrincipal.php
@@ -17,32 +17,99 @@ include("headGlobal.php");
 
 <main class="flex-grow-1 vh-100">
 
-    <div class="map-container">
-        <div  class="map" id="map"></div>
+
+    <div id="mapCarousel" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active" id="carouselItem1">
+                <div id="map1" class="leaflet-container"></div>
+            </div>
+            <div class="carousel-item" id="carouselItem2">
+                <div id="map2" class="leaflet-container"></div>
+            </div>
+            <div class="carousel-item" id="carouselItem3">
+                <div id="map3" class="leaflet-container"></div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#mapCarousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#mapCarousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        </button>
     </div>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet-gpx/1.7.0/gpx.min.js"></script>
+
     <script>
-        var map = L.map('map', {
-            center: [51.505, -0.09],
-            zoom: 13,
-            zoomControl: false,
-            dragging: false,
-            scrollWheelZoom: false
+        var map1, map2, map3;
+
+        $('#mapCarousel').on('slid.bs.carousel', function() {
+            initMaps();
         });
 
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '© OpenStreetMap contributors'
-        }).addTo(map);
+        function initMaps() {
+            if ($('#carouselItem1').hasClass('active')) {
+                if (!map1) {
+                    map1 = L.map('map1', {
+                        zoomControl: false,
+                        dragging: false,
+                        touchZoom: false,
+                        doubleClickZoom: false,
+                        boxZoom: false,
+                        scrollWheelZoom: false
+                    })
 
-        var gpxLayer = new L.GPX('../gpx/aw.gpx', {
-            async: true,
-        }).addTo(map);
+                    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                        attribution: '© OpenStreetMap contributors'
+                    }).addTo(map1);
 
-        gpxLayer.on("loaded", function(e) {
-            map.fitBounds(e.target.getBounds());
+                    // Load the GPX file onto map1
+                    var gpxLayer = new L.GPX('../gpx/aw.gpx', {
+                        async: true,
+                        marker_options: {
+                            startIconUrl: 'path/to/start/icon.png',
+                            endIconUrl: 'path/to/end/icon.png',
+                            shadowUrl: 'path/to/shadow.png',
+                        }
+                    }).on('loaded', function(e) {
+                        map1.fitBounds(e.target.getBounds());
+                    }).addTo(map1);
+                }
+            } else if ($('#carouselItem2').hasClass('active')) {
+                if (!map2) {
+                    map2 = L.map('map2', {
+                        zoomControl: false,
+                        dragging: false,
+                        touchZoom: false,
+                        doubleClickZoom: false,
+                        boxZoom: false,
+                        scrollWheelZoom: false
+                    }).setView([40.7128, -74.0060], 13);
+                    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                        attribution: '© OpenStreetMap contributors'
+                    }).addTo(map2);
+                }
+            } else if ($('#carouselItem3').hasClass('active')) {
+                if (!map3) {
+                    map3 = L.map('map3', {
+                        zoomControl: false,
+                        dragging: false,
+                        touchZoom: false,
+                        doubleClickZoom: false,
+                        boxZoom: false,
+                        scrollWheelZoom: false
+                    }).setView([34.0522, -118.2437], 13);
+                    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                        attribution: '© OpenStreetMap contributors'
+                    }).addTo(map3);
+                }
+            }
+        }
+
+        // Initial map initialization on page load
+        $(document).ready(function() {
+            initMaps();
         });
     </script>
+
 </main>
 <?php include("footer.php"); ?>
