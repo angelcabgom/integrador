12db4605382efc49018e944257e1ddf7d3a6f499
angelcabgom <angelm.cabgom.1@educa.jcyl.es>
2024-01-25 11:03:59 +0100

    Mapa con ruta funcionando supuestamente

--- CODE/vista/headGlobal.php
+++ CODE/vista/headGlobal.php
@@ -12,7 +12,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <link href="https://db.onlinewebfonts.com/c/e9de4f4d373ef5c6d9cf52490816ff5d?family=BMW+Type+Web+Light+All" rel="stylesheet">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.css">
+
     <!-- Array con todos los estilos en comun -->
     <?php $estilosComunes = [
         "style" => "../css/style.css",
@@ -30,12 +31,13 @@
     ?>
 </head>
 
-<?php
+<body>
+    <?php
 
-session_start();
-// Aqui se incluyen los headers
-$imagenPerfil = obtenerImagen();
-$GLOBALS['imagenPerfil'] = $imagenPerfil; // Se mete la imagen de perfil en un $GLOBALS para poder acceder desde cualquier archivo
-comprobarTipoSesion($nombreArchivo);
-session_write_close();
-?>
+    session_start();
+    // Aqui se incluyen los headers
+    $imagenPerfil = obtenerImagen();
+    $GLOBALS['imagenPerfil'] = $imagenPerfil; // Se mete la imagen de perfil en un $GLOBALS para poder acceder desde cualquier archivo
+    comprobarTipoSesion($nombreArchivo);
+    session_write_close();
+    ?>
--- CODE/vista/paginaPrincipal.php
+++ CODE/vista/paginaPrincipal.php
@@ -16,19 +16,33 @@ include("headGlobal.php");
 ?>
 
 <main class="flex-grow-1 vh-100">
-    <p>
-    <div id="map"></div>
-    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+
+    <div class="map-container">
+        <div  class="map" id="map"></div>
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet-gpx/1.7.0/gpx.min.js"></script>
     <script>
         var map = L.map('map', {
             center: [51.505, -0.09],
-            zoom: 13
+            zoom: 13,
+            zoomControl: false,
+            dragging: false,
+            scrollWheelZoom: false
         });
 
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '© OpenStreetMap contributors'
         }).addTo(map);
+
+        var gpxLayer = new L.GPX('../gpx/aw.gpx', {
+            async: true,
+        }).addTo(map);
+
+        gpxLayer.on("loaded", function(e) {
+            map.fitBounds(e.target.getBounds());
+        });
     </script>
-    </p>
 </main>
 <?php include("footer.php"); ?>
